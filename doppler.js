8da497abd0d34b2ee8ad9a9e1cccc9607071a6f1
Kerandor <tbridges024@gmail.com>
2022-04-10 18:30:19 -0500

    2

---
+++ doppler.js
@@ -0,0 +1,3 @@
+e = () =>osc(7,0.05).modulate(noise(2), 0.53).diff(o0).modulateRotate(osc(2).modulate(voronoi(5,0.2).scrollY(), 0.42))
+
+osc(1).modulateKaleid(e()).color(2,1.5,1).colorama(0.0004).scale(.5).out(o0)
